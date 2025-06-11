f84db30d93a3d04e2554cb960c3c33d186864e07
sravankumarsaiteja@gmail.com <151546042+Sravan-25@users.noreply.github.com>
2025-06-11 11:48:00 +0530

    Update the ui and add new features

--- script.js
+++ script.js
@@ -4,8 +4,14 @@ let newBtn = document.querySelector('#new-btn');
 let msgContainer = document.querySelector('.msg-container');
 let msg = document.querySelector('#msg');
 let draw = document.querySelector('#draw');
+let soundBtn = document.querySelector('#sound-btn');
+let clickSound = document.querySelector('#click-sound');
+let winSound = document.querySelector('#win-sound');
+let drawSound = document.querySelector('#draw-sound');
 
 let turnO = true;
+let moveCount = 0;
+let soundOn = true;
 
 const patterns = [
   [0, 1, 2],
@@ -20,57 +26,81 @@ const patterns = [
 
 boxes.forEach((box) => {
   box.addEventListener('click', () => {
+    if (box.innerHTML !== '') return;
+    if (soundOn) clickSound.play();
     if (turnO) {
-      box.innerText = 'O';
-      box.style.color = 'red';
+      box.innerHTML = 'O';
+      box.classList.add('o');
       turnO = false;
     } else {
-      box.innerText = 'X';
+      box.innerHTML = 'X';
+      box.classList.add('x');
       turnO = true;
-      box.style.color = 'white';
     }
     box.disabled = true;
-
-    chackWinner();
+    moveCount++;
+    checkWinner();
   });
 });
 
 const showWinner = (winner) => {
-  msg.innerHTML = `Congratulations, Winner is ${winner}`;
+  msg.innerHTML = `🎉 Player ${winner} Wins! 🎉`;
   msgContainer.classList.remove('hide');
-  disabledBoxes();
+  if (soundOn) winSound.play();
+  disableBoxes();
 };
 
-const chackWinner = () => {
+const showDraw = () => {
+  draw.innerHTML = `🤝 It's a Draw! 🤝`;
+  msgContainer.classList.remove('hide');
+  if (soundOn) drawSound.play();
+  disableBoxes();
+};
+
+const checkWinner = () => {
+  let isWinner = false;
   for (let pattern of patterns) {
     let pos1Val = boxes[pattern[0]].innerHTML;
     let pos2Val = boxes[pattern[1]].innerHTML;
     let pos3Val = boxes[pattern[2]].innerHTML;
 
-    if (pos1Val != '' && pos2Val != '' && pos3Val != '') {
+    if (pos1Val !== '' && pos2Val !== '' && pos3Val !== '') {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
+        isWinner = true;
+        break;
       }
     }
   }
+  if (!isWinner && moveCount === 9) {
+    showDraw();
+  }
 };
 
-const disabledBoxes = () => {
-  for (let box of boxes) {
-    box.disabled = true;
-  }
+const disableBoxes = () => {
+  boxes.forEach((box) => (box.disabled = true));
 };
-const enabledBoxes = () => {
-  for (let box of boxes) {
+
+const enableBoxes = () => {
+  boxes.forEach((box) => {
     box.disabled = false;
     box.innerHTML = '';
-  }
+    box.classList.remove('o', 'x');
+  });
 };
+
 const resetGame = () => {
   turnO = true;
-  enabledBoxes();
+  moveCount = 0;
+  enableBoxes();
   msgContainer.classList.add('hide');
+};
+
+const toggleSound = () => {
+  soundOn = !soundOn;
+  soundBtn.innerHTML = soundOn ? '🔊 Sound On' : '🔇 Sound Off';
 };
 
 newBtn.addEventListener('click', resetGame);
 reset.addEventListener('click', resetGame);
+soundBtn.addEventListener('click', toggleSound);
