7dfac4c9c61d12699737540c649a2f654af85857
Sravan Kumar Reddy <151546042+Sravan-25@users.noreply.github.com>
2025-06-11 12:44:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ const checkWinner = () => {
     if (pos1Val !== '' && pos2Val !== '' && pos3Val !== '') {
       if (pos1Val === pos2Val && pos2Val === pos3Val) {
         showWinner(pos1Val);
-        return; // Stop further checking
+        return; 
       }
     }
   }
